e562ada2bdffa0090b0113962be18ebaf90e0db8
= <miguel.arguedas.mejias@gmail.com>
2018-03-22 10:22:53 -0600

    adding settings of mongodb

--- django_tags_sga/settings.py
+++ django_tags_sga/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'django_tags_sga.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': ('sga_mongo'),
     }
 }
 
